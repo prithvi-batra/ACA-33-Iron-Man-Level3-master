ce77578eeb907f9306fde988c8cba5de7ef12976
prithvi-batra <89774123+prithvi-batra@users.noreply.github.com>
2021-10-27 22:18:41 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,83 @@
+var bg, backgroundImg ;
+var stone ;
+var stoneGroup;
+var diamondGroup;
+function preload() {
+  bgAnimation  = loadAnimation("images/bg.jpg","images/bg.jpg","images/bg.jpg");
+  ironImg      = loadImage("images/iron.png");
+  stoneImage   = loadImage("images/stone.png");
+  diamondImage = loadImage("images/diamond.png");
+}
+
+function setup() {
+  createCanvas(1500, 600);
+  bg   = createSprite(300,300,300,300);
+  iron = createSprite(100,450);
+  bg.addAnimation("bgAnimation",bgAnimation);
+  bg.scale = 2;
+  iron.addImage(ironImg);
+  iron.scale = 0.4;
+  bg.velocityY = 4;
+  edges=createEdgeSprites();
+  stoneGroup   = new Group();
+  diamondGroup = new Group();
+}
+
+function draw() {
+  background('black');  
+  iron.bounceOff(edges[0]);
+  iron.bounceOff(edges[1]);
+  iron.bounceOff(edges[2]);
+  iron.bounceOff(edges[3]);
+  iron.debug = true;
+  if(bg.y > 750 ){
+    bg.y = 0;
+  }
+  if (keyDown("up")){
+    iron.y = iron.y -18 ;
+  }
+  if (keyDown("down")){
+    iron.y = iron.y +18 ;
+  }
+  if (keyDown("left")){
+    iron.x = iron.x -18 ;
+  }
+  if (keyDown("right")){
+    iron.x = iron.x +18 ;
+  }
+  for( var i = 0 ; i<stoneGroup.length; i++){
+    var temp = (stoneGroup).get(i);
+    if(temp.isTouching(iron)){
+        iron.collide(temp);
+    }
+}
+  iron.setCollider("rectangle",100,-60,180,400);
+  for(var i = 0 ; i<diamondGroup.length ; i++){
+    var temp = (diamondGroup).get(i);
+    if(temp.isTouching(iron)){
+      temp.destroy();
+    }
+  }
+  generateDiamonds();
+  generateStones();
+  drawSprites();   
+}
+function generateStones(){
+  if(frameCount%100 == 0){
+    stone = createSprite(random(150,1500),0,40,10);
+    stone.addImage(stoneImage)
+    stone.scale = 0.8;
+    stone.velocityY = 4;
+    stone.lifetime = 350;
+    stoneGroup.add(stone);
+  }
+}
+function generateDiamonds(){
+  if(frameCount%40 == 0){
+    diamond = createSprite(random(150,1500),0,40,10);
+    diamond.addImage(diamondImage);
+    diamond.velocityY = 4;
+    diamond.lifetime = 350;
+    diamondGroup.add(diamond);
+  }
+} 
